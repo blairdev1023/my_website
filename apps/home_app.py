dafde0b63835df8944d99bd899e8023a6b296bcb
Blair Thurman <blairdev1023@gmail.com>
2018-11-17 22:02:41 -0700

    added about and projects

--- apps/home_app.py
+++ apps/home_app.py
@@ -5,10 +5,9 @@ from dash.dependencies import Input, Output
 from app import app
 
 layout = html.Div([
-    html.Div(
-        className='app-header',
-        children=[html.Div(className='app-header--title')]
-    ),
+    # Page Header
+    html.Div(className='app-header'),
+    # Headshot
     html.Div(
         html.Div(children=html.Img(
             className='picture',
@@ -32,6 +31,7 @@ layout = html.Div([
             backgroundColor='white'
         )
     ),
+    # Title
     html.Div(html.Div([
         html.P('Hello World,', className='title-text'),
         html.P('I\'m Blair Thurman!', className='title-text')
@@ -42,6 +42,71 @@ layout = html.Div([
             float='left',
             display='inline-block',
             backgroundColor='white'
+        )
+    ),
+    # About
+    html.Div(
+        html.Div('About:', className='section-header'),
+        style=dict(
+            height=200,
+            width='35%',
+            float='left',
+            display='inline-block',
+            backgroundColor='white',
+        )
+    ),
+    html.Div([
+        html.P(className='text-block', children=
+        '''
+        I'm an unabashed data nerd, father of two cats, wannabe historian, and an awful dancer. This site serves as my portfolio for my passion projects.
+        '''),
+        html.P(className='text-block', children=
+        '''
+        Born and raised in Colorado, I spent 2018 exploring and getting lost in the Western United States. Now that I've satiated my wanderlust I'm setting down roots. Being the sun-creature that I am - I've decided on Phoenix!
+        ''')],
+        style=dict(
+            height=200,
+            width='65%',
+            float='left',
+            display='inline-block',
+            backgroundColor='white',
+        )
+    ),
+    # Project
+    # About
+    html.Div(
+        html.Div('Projects:', className='section-header'),
+        style=dict(
+            height=200,
+            width='35%',
+            float='left',
+            display='inline-block',
+            backgroundColor='white',
+        )
+    ),
+    html.Div([
+
+        html.A(
+            html.Button('Gun Violence'),
+            href='/gun_violence'
+        ),
+        html.A(
+            html.Button('Telco'),
+            href='/telco'
+        ),
+
+        html.P(className='text-block', children=
+        '''
+        '''),
+        html.P(className='text-block', children=
+        '''
+        ''')],
+        style=dict(
+            height=200,
+            width='65%',
+            float='left',
+            display='inline-block',
+            backgroundColor='white',
         )
     )
 ], style={'width': '80%', 'margin': 'auto', 'backgroundColor': 'white'})
